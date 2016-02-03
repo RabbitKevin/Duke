969e2597b1294d6aa3516a67b17ce30cdf03e068
Kevin Yang <ykwwind@gmail.com>
2016-02-03 02:27:04 -0800

    Step2 completed & Step3 files added

--- Part5/SecondRatings.java
+++ Part5/SecondRatings.java
@@ -1,8 +1,8 @@
 
 /**
  * Write a description of SecondRatings here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 
@@ -11,10 +11,54 @@ import java.util.*;
 public class SecondRatings {
     private ArrayList<Movie> myMovies;
     private ArrayList<Rater> myRaters;
-    
+
     public SecondRatings() {
         // default constructor
-        this("ratedmoviesfull.csv", "ratings.csv");
     }
-    
+    public SecondRatings(String moviefile, String ratingsfile) {
+        FirstRatings tmp = new FirstRatings();
+        myMovies = tmp.loadMovies(moviefile);
+        myRaters = tmp.loadRaters(ratingsfile);
+    }
+    public int  getMovieSize() {
+        return myMovies.size();
+    }
+    public int getRaterSize() {
+        return myRaters.size();
+    }
+    public ArrayList<Rating> getAverageRatings(int minimalRaters){
+        ArrayList<Rating> ratings = new ArrayList<Rating>();
+        for(Movie movie : myMovies){
+            String id = movie.getID();
+            double average = getAverageByID(id, minimalRaters);
+            if(average != 0.0){
+                ratings.add(new Rating(id, average));
+            }
+        }
+        return ratings;
+    }
+    public String getTitle(String id){
+        for(Movie movie : myMovies){
+            if(movie.getID().equals(id)) return movie.getTitle();
+        }
+        return "No such movie";
+    }
+    public String getID(String title){
+        for(Movie movie : myMovies){
+            if(movie.getTitle().equals(title)) return movie.getID();
+        }
+        return "No such ID";
+    }
+    private double getAverageByID(String id, int minimalRaters){
+        int count = 0;
+        double total = 0.0;
+        for(Rater rater : myRaters){
+            if(rater.hasRating(id)){
+                total+=rater.getRating(id);
+                count++;
+            }
+        }
+        if(count < minimalRaters) return 0.0;
+        return total/count;
+    }
 }
--- Part5/test.java
+++ Part5/test.java
@@ -5,11 +5,14 @@ import org.apache.commons.csv.*;
 class test{
     public static void main(String[] args){
         //String movie = "data/ratedmoviesfull.csv";
-        String rate = "data/ratings.csv";
-        FirstRatings rating = new FirstRatings();
+        //String rate = "data/ratings.csv";
+        //FirstRatings rating = new FirstRatings();
         //ArrayList<Movie> movies = rating.loadMovies(movie);
         //rating.testLoadMovies(movies);
-        ArrayList<Rater> raters = rating.loadRaters(rate);
-        rating.testLoadRaters(raters);
+        //ArrayList<Rater> raters = rating.loadRaters(rate);
+        //rating.testLoadRaters(raters);
+        MovieRunnerAverage test = new MovieRunnerAverage();
+        test.printAverageRatings(12);
+        //test.getAverageRatingOneMovie();
     }
 }
